f5cfb2df5ef9c6725edcdcfe7b806200b5dcf49b
Eric Miller <ericmilleronline@gmail.com>
2014-09-22 20:24:41 -0400

    Added analytics tracking

--- README.md
+++ README.md
@@ -2,5 +2,3 @@ trimark-design-preview-app
 ==========================
 
 A Wordpress theme that controls the interface and admin panel of the TriMark Design Preview App
-
-Laptop Edit
--- header.php
+++ header.php
@@ -26,6 +26,7 @@ if($pg == 'front page') {
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 
 	<title>TriMark Digital - Projects Preview App</title>
+	<meta name="robots" content="noindex, nofollow" />
 
 	<?php /*
 	<meta name="HandheldFriendly" content="True">
@@ -39,6 +40,17 @@ if($pg == 'front page') {
 	<![endif]-->
 
 	<?php wp_head(); ?>
+
+	<?php // Google Analytics - 09/22/2014 ?>
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		ga('create', 'UA-4076497-21', 'auto');
+		ga('send', 'pageview');
+	</script>
+
 </head>
 <body <?php body_class(); ?>>
 
